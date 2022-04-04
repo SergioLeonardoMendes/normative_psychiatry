c34cd033e28042587736ab5a1db1c5e548474dbc
sergioleonardomendes <sergioleonardomendes@gmail.com>
2022-04-04 15:43:27 -0300

    matlab spm12 preprocessing scripts

---
+++ src/preprocessing/rawtovbm/abcd/start_spm12_job1_x07.m
@@ -0,0 +1,9 @@
+% List of open inputs
+nrun = 1; % enter the number of runs here
+jobfile = {'/data/spm12_job1_x07.m'};
+jobs = repmat(jobfile, 1, nrun);
+inputs = cell(0, nrun);
+for crun = 1:nrun
+end
+spm('defaults', 'PET');
+spm_jobman('run', jobs, inputs{:});
