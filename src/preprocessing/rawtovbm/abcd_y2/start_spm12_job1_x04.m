e86f8bf94c7286e570ccfbb8278198e2dcc740dd
sergioleonardomendes <sergioleonardomendes@gmail.com>
2024-02-22 14:42:41 -0300

    clean unused old files

--- src/preprocessing/rawtovbm/abcd_y2/start_spm12_job1_x04.m
+++
@@ -1,9 +0,0 @@
-% List of open inputs
-nrun = 1; % enter the number of runs here
-jobfile = {'/data/spm12_job1_x04.m'};
-jobs = repmat(jobfile, 1, nrun);
-inputs = cell(0, nrun);
-for crun = 1:nrun
-end
-spm('defaults', 'PET');
-spm_jobman('run', jobs, inputs{:});
